d7d04f6b1e77b66ee9d146d7262ade1382a50d68
Joey <43118582+JesusJoey@users.noreply.github.com>
2019-02-25 17:44:32 -0500

    Add files via upload

--- pytest.py
+++ pytest.py
@@ -1,16 +1,16 @@
 import subprocess
 import json
 import os
-class TestClass(object):
-    def test_one(self):
-        while not (os.path.exists('./video/out480p.mp4')):
-            pass
-        info_in = subprocess.check_output(['ffprobe', '-v', 'warning', '-print_format', 'json', '-show_streams',
+
+def test_one(self):
+    while not (os.path.exists('./output-video/IMG_0753.480.mp4')):
+        pass
+    info_in = subprocess.check_output(['ffprobe', '-v', 'warning', '-print_format', 'json', '-show_streams',
                                            '-show_format', 'newvideo.mp4'])  # check output
-        info_in = json.loads(info_in)
-        info_out = subprocess.check_output(['ffprobe', '-v', 'warning', '-print_format', 'json', '-show_streams',
+    info_in = json.loads(info_in)
+    info_out = subprocess.check_output(['ffprobe', '-v', 'warning', '-print_format', 'json', '-show_streams',
                                             '-show_format', './video/out480p.mp4'])
-        info_out = json.loads(info_out)
-        orig_duration = float(info_in['streams'][0]['duration'])  # check if there is any difference
-        new_duration = float(info_out['streams'][0]['duration'])
-        assert orig_duration == new_duration
+    info_out = json.loads(info_out)
+    orig_duration = float(info_in['streams'][0]['duration'])  
+    new_duration = float(info_out['streams'][0]['duration'])
+    assert orig_duration == new_duration
