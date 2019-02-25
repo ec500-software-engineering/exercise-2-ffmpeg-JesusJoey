35a9d1d81ef521c25b20b7c240698b56ce204993
Joey <43118582+JesusJoey@users.noreply.github.com>
2019-02-25 18:19:07 -0500

    Update test_output.py

--- test_output.py
+++ test_output.py
@@ -1,27 +1,10 @@
-from math import isclose
+from pytest import approx
+import subprocess
 
-def ffprobe(file_name)->dict:
-    """ get media metadata """
-    meta = subprocess.check_output(['ffprobe', '-v', 'warning',
-                                    '-print_format', 'json',
-                                    '-show_streams',
-                                    '-show_format',
-                                    file_name])
-    return json.loads(meta)
 
 def test_duration():
-    fnin = 'video.mp4'
-    fnout1 = 'video_480p.mp4'
-    fnout2 = 'video_720p.mp4'
-
-    orig_meta = ffprobe(fnin)
-    orig_duration = float(orig_meta['streams'][0]['duration'])
-    meta_480 = ffprobe(fnout1)
-    meta_720 = ffprobe(fnout2)
-    duration_480 = float(meta_480['streams'][0]['duration'])
-    duration_720 = float(meta_720['streams'][0]['duration'])
-    assert isclose(orig_duration, duration_480, abs_tol=1)
-    assert isclose(orig_duration, duration_720, abs_tol=1)
-    print('all successful!')
-
-test_duration()
+    inp='.output-video/IMG_0753.480.mp4'
+    out='.output-video/IMG_0753.720.mp4'
+    outLen = subprocess.call(['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1',out])
+    inLen  = subprocess.call(['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1',inp])
+    assert inLen == approx(outLen)
