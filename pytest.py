3727254e224924f8b7e52be50d63f81457127e61
Joey <43118582+JesusJoey@users.noreply.github.com>
2019-02-25 17:39:09 -0500

    Add files via upload

--- pytest.py
+++ pytest.py
@@ -1,9 +1,16 @@
-import main
-from pytest import approx
-
-def test_ffmpeg():
-	assert 1==1
-    print("All files are converted successfully!")
-
-if __name__ == '__main__':
-    test_ffmpeg()
+import subprocess
+import json
+import os
+class TestClass(object):
+    def test_one(self):
+        while not (os.path.exists('./video/out480p.mp4')):
+            pass
+        info_in = subprocess.check_output(['ffprobe', '-v', 'warning', '-print_format', 'json', '-show_streams',
+                                           '-show_format', 'newvideo.mp4'])  # check output
+        info_in = json.loads(info_in)
+        info_out = subprocess.check_output(['ffprobe', '-v', 'warning', '-print_format', 'json', '-show_streams',
+                                            '-show_format', './video/out480p.mp4'])
+        info_out = json.loads(info_out)
+        orig_duration = float(info_in['streams'][0]['duration'])  # check if there is any difference
+        new_duration = float(info_out['streams'][0]['duration'])
+        assert orig_duration == new_duration
