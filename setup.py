2c5da5d492719cb129817864bd10f33ddeb9eca8
Joey <43118582+JesusJoey@users.noreply.github.com>
2019-02-25 17:10:02 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name='exercise-2-ffmpeg-JesusJoey',
+	version='',
+	description='ffmpeg exercise',
+	author='Yang Qiao',
+	author_email='joeyang@bu.edu',
+	url='',
+	packages=[''],
+)
